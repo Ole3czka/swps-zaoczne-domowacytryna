56d216fd547d537e0dfce3a3a24f7d90b234c397
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-12-14 14:42:58 +0100

    Store allowed signs in a list

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,4 +1,6 @@
-﻿// // See https://aka.ms/new-console-template for more information
+﻿// typ nazwa;
+// typ nazwa = wartosc;
+List<string> allowedSigns = ["rock", "paper", "scissors"];
 
 Console.WriteLine("Let's play Rock Paper Scissors, ok?");
 
@@ -9,9 +11,9 @@ if (firstSign.Equals(secondSign, StringComparison.OrdinalIgnoreCase))
 {
     Console.WriteLine("It's a tie!");
 }
-else if ((firstSign == "rock" && secondSign == "scissors")
-    || (firstSign == "paper" && secondSign == "rock")
-    || (firstSign == "scissors" && secondSign == "paper"))
+else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2])
+    || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0])
+    || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
 {
     Console.WriteLine("First player won!");
 }
@@ -22,13 +24,15 @@ else
 
 string GetCorrectSign(string playerName)
 {
-    Console.WriteLine($"{playerName}, provide sign (rock/paper/scissors)");
+    // Console.WriteLine($"{playerName}, provide sign ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
+    Console.WriteLine($"{playerName}, provide sign ({string.Join("/", allowedSigns)})");
     string sign = Console.ReadLine()!;
     // czy podany znak NIE jest jednym z oczekiwanych?
-    while (!(sign == "rock" || sign == "paper" || sign == "scissors"))
+    // while (!(sign == allowedSigns[0] || sign == allowedSigns[1] || sign == allowedSigns[2]))
+    while (!allowedSigns.Contains(sign))
     {
         // wyświetl ze znak jest niepoprawny
-        Console.WriteLine("Provide correct sign! (rock/paper/scissors)");
+        Console.WriteLine($"Provide correct sign! ({string.Join("/", allowedSigns)})");
         // pozwól poprawić swoje zachowanie (poda ponownie znak)
         sign = Console.ReadLine()!;
     }
